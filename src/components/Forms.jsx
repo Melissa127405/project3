92578b9e030f243f6c3630444de6cde16cd94a03
Melissa127405 <melissastackpole@gmail.com>
2026-01-28 16:22:14 -0500

    states

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,33 +1,60 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import React,{useState} from 'react';
 
 
 function Forms () {
+    const [doodleName, setDoodleName] = useState(
+        {   doodleName: '',
+            doodleType: '',
+            firstLast: '',
+            contactEmail: ''
+        }
+    )
+    function handleChange(event) {
+      console.log (event.target.name);
+    }
+    
 
     return (
         <form> 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Doodle's Name</Form.Label>
-        <Form.Control type="name" placeholder="Enter Name" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Control 
+        type="text"  
+        name="doodleName"
+        value={doodleName.doodleName}
+        onChange={handleChange}
+        />
+      
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>What Type of Doodle?</Form.Label>
-        <Form.Control type="type of " placeholder="Doodle Type" />
-      </Form.Group>
+        <Form.Control 
+        type="text "  
+        name="doodleType"
+        value={doodleName.doodleType}
+        />
+        </Form.Group>
       
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Your First and Last Name</Form.Label>
-        <Form.Control type="first last  " placeholder="First , Last " />
+        <Form.Control 
+        type="text"
+        name="firstLast"
+        value={doodleName.firstLast}
+        />
       </Form.Group>
       
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Contact Email </Form.Label>
-        <Form.Control type="type of " placeholder="Email" />
+        <Form.Control 
+        type="text" 
+        name="contactEmail"
+        value={doodleName.contactEmail}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="We will be in touch asap!" />
